3524ff45d19f996689fd4e25c21ac2c535375103
yjjnls <x-jj@foxmail.com>
2019-02-27 19:58:18 +0800

    add env.sh for debug

---
+++ env.sh
@@ -0,0 +1,4 @@
+cd /root/gstreamer/linux_x86_64/ && source tshell.sh && cd -
+export FONTCONFIG_PATH=/etc/fonts
+export LD_LIBRARY_PATH=:/root/Desktop/del/napi-emscripten/test/StreamMatrix/build/
+export GST_DEBUG=stream_matrix:5
